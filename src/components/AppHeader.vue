d8a9953fba6d672642a95cc6c2bfebee189b2bf4
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-10 17:21:58 +0200

    aggiunta array in header e logo+li

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,39 @@
 <template>
     <header>
-    <h1>Header Content</h1>
+        <section class="logo">
+            <img src="../assets/img/img/dc-logo.png" alt="logo">
+        </section>
+        <nav>
+            <ul>
+                <li v-for="(menuItem, index) in menuItems" :key="index">
+                    <a :href="menuItem.url">{{ menuItem.text }}</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'AppHeader'
-}
+    data() {
+        return {
+            menuItems: [
+                { text: 'Characters', url: '#'},
+                { text: 'Comics', url: '#' },
+                { text: 'Movies', url: '#' },
+                { text: 'Tv', url: '#' },
+                { text: 'Games', url: '#' },
+                { text: 'Collectibles', url: '#' },
+                { text: 'Videos', url: '#' },
+                { text: 'Fans', url: '#' },
+                { text: 'News', url: '#' },
+                { text: 'Shop', url: '#' }
+            ]
+        };
+    }
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+
 </style>
