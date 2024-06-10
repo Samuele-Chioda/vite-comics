2486665e661819d68d6751cd19a0d528eb2e98e8
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-10 19:26:31 +0200

    aggiunta footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,17 @@
 <template>
     <footer>
         <section class="left-sec">
-            <button></button>
+            <button>Sign-up now!</button>
         </section>
         <section class="right-sec">
             <h1>
                 Follow us
             </h1>
-
+            <img src="../assets/img/img/footer-facebook.png" alt="">
+            <img src="../assets/img/img/footer-periscope.png" alt="">
+            <img src="../assets/img/img/footer-pinterest.png" alt="">
+            <img src="../assets/img/img/footer-twitter.png" alt="">
+            <img src="../assets/img/img/footer-youtube.png" alt="">
         </section>
     </footer>
 </template>
@@ -20,8 +24,37 @@ export default {
 
 <style lang="scss" scoped>
 
-footer{
+footer {
     background-color: #303030;
+    display: flex;
+    justify-content: space-between;
+    padding: 4rem;
+}
+
+button {
+    padding: 1rem;
+    background-color: #303030;
+    border: 3px solid #0282f9;
+    color: white;
+    text-transform: uppercase;
+    font-size: 1.2rem;
+}
+
+h1{
+    text-transform: uppercase;
+    color: #0282f9;
+    margin-right: .5rem;
+    padding:0.5rem;
+}
+
+.right-sec{
+    display: flex;
+}
+
+img{
+    margin-right: .5rem;
+    width: 100%;
+    padding:.5rem
 }
 
 </style>
